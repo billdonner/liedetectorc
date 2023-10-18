88715ed60392d7426b94fcd7d0e263fc3aca3d24
bill donner <billdonner@gmail.com>
2023-10-18 10:27:30 -0500

    0.5.0

--- Package.swift
+++ Package.swift
@@ -5,8 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "liedetectorc",
+    platforms: [
+      .macOS(.v14),
+           .iOS(.v16)
+       ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package (url:"https://github.com/billdonner/q20kshare", from: "0.4.91")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -15,6 +20,8 @@ let package = Package(
             name: "liedetectorc",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                
+                .product(name: "q20kshare", package: "q20kshare"),
             ]
         ),
     ]
---
+++ Sources/main.swift
@@ -0,0 +1,171 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+//
+//  main.swift
+//  veracitatorr on 6/25/23.
+//
+
+//Step 3: liedetector executes a script file from Prepper, sending each prompt to (another) Chatbot and generates a single output file of JSON data which is read by Blender."
+
+
+import Darwin
+import Foundation
+import q20kshare
+import ArgumentParser
+
+let ChatGPTModel = "text-davinci-003"
+let ChatGPTURLString = "https://api.openai.com/v1/completions"
+
+public  func getOpinions(_ item:String,source:String) throws -> [Opinion] {
+  guard  item != "" else { print("** nothing found in getOpinions from \(item)"); return [] }
+  var xitem = item
+  if xitem.last != "]" {
+    xitem += "]"
+  }
+  var opinions:[Opinion] = []
+  do {
+    let aiopinions = try JSONDecoder().decode([AIOpinion] .self,from:xitem.data(using:.utf8)!)
+    opinions =  aiopinions.map { $0.toOpinion(source: source)}
+  }
+  catch {
+    do {
+      let aiopinions = try JSONDecoder().decode([AIAltOpinion] .self,from:xitem.data(using:.utf8)!)
+      opinions = aiopinions.map {$0.toOpinion(source: source)}
+    }
+    catch {
+      print("*** No opinion found \(error)\n item: '\(item)'")
+    }
+  }
+  return opinions
+}
+
+func handleItems(ctx:ChatContext,item:String,jsonOut:FileHandle?) throws {
+  let opinions = try getOpinions(item, source:ChatGPTModel)
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted
+  
+ for opinion  in opinions {
+    // 3. write JSON to file
+    if let fileHandle = jsonOut  {
+      // append response with prepended comma if we need one
+      if !ctx.first {
+        fileHandle.write(",".data(using: .utf8)!)
+      } else {
+        ctx.first  = false
+      }
+      // 4. encode Challenge as JSON and write that out
+      let data = try encoder.encode(opinion)
+      let str = String(data:data,encoding: .utf8)
+      if let str = str {
+        fileHandle.write(str.data(using: .utf8)!)
+      }
+    }
+  }
+}
+struct LieDetector: ParsableCommand {
+  
+  static let configuration = CommandConfiguration(
+    abstract: "Step 3: liedetector executes a script file from Prepper, sending each prompt to (another) Chatbot and generates a single output file of JSON data which is read by Blender.",
+    version: "0.5.0",
+    subcommands: [],
+    defaultSubcommand: nil,
+    helpNames: [.long, .short]
+  )
+  
+  @Argument(help: "Input text script file (Between_2_3.txt)")
+  var input: String
+  
+  @Argument( help:"Output json file (Between_3_4.json)")
+  var output: String
+  
+  
+  @Option(name: .long, help: "How many prompts to execute")
+  var max: Int = 65535
+  
+  @Option(name: .long, help: "Print dots whilst awaiting AI")
+  var dots: Bool = false
+  
+  @Option(name: .long, help: "Print a lot more")
+  var verbose: Bool = false
+  
+  @Option(name: .long , help: "Generate Unique File Names")
+  var unique : Bool = true
+  
+  @Option(name: .long, help: "Don't call AI")
+  var dontcall: Bool = false
+  
+  @Option(name:.long,help: "The pattern to use to split the file")
+  var split_pattern: String = "***"
+  
+  @Option(name:.long,help: "The pattern to use to indicate a comments line")
+  var comments_pattern: String = "///"
+  
+  
+  mutating func run() throws {
+    print(">Liedetector Command Line: \(CommandLine.arguments)")
+    print(">Liedetector is running at \(Date())")
+    
+    let apiKey = try getAPIKey()
+    
+    print(">Liedetector Using apikey: " + apiKey)
+    guard let apiurl = URL(string: ChatGPTURLString) else {
+      fatalError("Invalid API URL")
+    }
+    guard let outURL = URL(string:output) else {
+      fatalError("Invalid Output URL")
+    }
+    let ctx = ChatContext(max:max,apiKey:apiKey, apiURL: apiurl, outURL: outURL, model:ChatGPTModel, verbose:verbose, dots:dots,dontcall:dontcall,comments_pattern:comments_pattern,split_pattern:split_pattern, style:.promptor)
+    
+    guard let url = URL(string:input) else {  print ("bad url"); return  }
+    let contents = try String(contentsOf: url)
+    let templates = contents.split(separator: split_pattern)
+      .map{$0.trimmingCharacters(in: .whitespacesAndNewlines)}
+    if  verbose {
+      print(">Prompts url: \(url)  (\(contents.count) bytes, \(templates.count) templates)")
+      print(">Contacting: \(ChatGPTURLString)")
+    }
+    
+    
+    let jsonOutHandle = try prepOutputChannels(ctx:ctx)
+    do {
+      if let jsonOutHandle = jsonOutHandle {
+        try pumpItUp(ctx:ctx,templates:templates, jsonOut: jsonOutHandle, justOnce:true, cleaner: {s in
+        let zz = s.trimmingCharacters(in: .whitespacesAndNewlines)
+         let xx =  String(zz.dropLast(2))
+         // print(xx)
+          return [xx]
+        },itemHandler: { x,y,z in
+          try handleItems(ctx: x, item: y, jsonOut: z)
+        })
+      }
+    }
+    catch {
+      if error as? PumpingErrors == PumpingErrors.reachedMaxLimit {
+        print("\n>Liedetector reached max limit of \(ctx.max) prompts sent to the AI")
+        print(">Liedetector Exiting Normally - Pumped:\(ctx.pumpCount)" + " Bad Json: \( ctx.badJsonCount)" + " Network Issues: \(ctx.networkGlitches)\n")
+        LieDetector.exit()
+      } else
+      if error as? PumpingErrors == PumpingErrors.reachedEndOfScript {
+        print("\n>Liedetector reached end of input script \(ctx.pumpCount) prompts sent to the AI")
+        print(">Liedetector Exiting Normally - Pumped:\(ctx.pumpCount)" + " Bad Json: \( ctx.badJsonCount)" + " Network Issues: \(ctx.networkGlitches)\n")
+        LieDetector.exit()
+      }
+      else {
+        print ("Unknown error: \(error)")
+        LieDetector.exit()
+      }
+    }
+    
+    
+    if ctx.pumpCount < ctx.max  {
+      RunLoop.current.run() // suggested by fivestars blog
+    }
+    print(">Liedetector Exiting Normally - Pumped:\(ctx.pumpCount)" + " Bad Json: \( ctx.badJsonCount)" + " Network Issues: \(ctx.networkGlitches)\n")
+  }// otherwise we should exit
+}
+
+LieDetector.main()
+
